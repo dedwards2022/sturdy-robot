3624eef97a308e50ec80ce23ad502580dffa4e8e
danieledwards2022 <daniel.edwards2019@outlook.com>
2024-01-13 22:41:28 -0500

    initial commit

---
+++ csc500_critical_thinking_week1_part2/main.py
@@ -0,0 +1,12 @@
+# Name of Application
+print("Multiplication & Division\n")
+
+# User inputs first number
+num1 = int(input("Input First Number: "))
+# User inputs second number
+num2 = int(input("Input Second Number: "))
+
+# Multiplying and outputting to console
+print("num1 * num2 =", num1 * num2)
+# Dividing and outputting to console
+print("num1 / num2 =", num1 / num2)
